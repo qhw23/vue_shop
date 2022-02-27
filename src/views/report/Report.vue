a952339b331f291c835ac8a09dfb52ce4eec06b4
qhw23 <2650064822@qq.com>
2022-02-27 15:43:55 +0800

    完成

--- src/views/report/Report.vue
+++ src/views/report/Report.vue
@@ -16,42 +16,64 @@
 </template>
 
 <script>
-// 1.导入 echarts
-import echarts from 'echarts'
+// 1.引用echarts组件
+import * as echarts from 'echarts';
+//导入组件
+import _ from 'lodash'
 
 export default {
   props: {},
   data() {
-    return {}
+    return {
+      // 需要合并的数据
+      options: {
+        title: {
+          text: '用户来源',
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3',
+            },
+          },
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            boundaryGap: false,
+          },
+        ],
+        yAxis: [
+          {
+            type: 'value',
+          },
+        ],
+      },
+    }
   },
   //  此时页面上的元素 已经渲染完毕
-  mounted() {
-    //   3.基于准备好的dom 初始化echarts实例
-    var myChart = echarts.init(document.getElementById(' main'))
+  async mounted() {
+    // 3.基于准备好的dom，初始化echarts实例  挂载前初始化
+    var myChart = echarts.init(document.getElementById('main'))
 
-    // 4.准备数据和配置项
-    var option = {
-      title: {
-        text: 'ECharts 入门示例',
-      },
-      tooltip: {},
-      legend: {
-        data: ['销量'],
-      },
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
-      },
-      yAxis: {},
-      series: [
-        {
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20],
-        },
-      ],
+    // 发起请求 获取后台数据
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status != 200) {
+      return this.$message.error('获取折线图数据失败')
     }
-    // 5展示数据
-    myChart.setOption(option)
+
+    // 4.准备数据和配置项 合并数据
+    const result = _.merge(res.data, this.options)
+
+    // 绘制图表
+    myChart.setOption(result)
   },
   methods: {},
 }
